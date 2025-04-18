ac4ee1e9934664da7cdfdca5a7c67c23f289cb9a
oyj <979109228@qq.com>
2025-04-18 18:01:33 +0800

    add study demo
    Signed-off-by: oyj <979109228@qq.com>

---
+++ llm_app_GLM.py
@@ -0,0 +1,13 @@
+from zhipuai import ZhipuAI
+client = ZhipuAI(api_key="c7627a84cc2d4e1fb660498f84642cd6.u6UlTBBQ5UR9PaqF")  # 请填写您自己的APIKey
+response = client.chat.completions.create(
+    model="glm-4-plus",  # 请填写您要调用的模型名称
+    messages=[
+        {"role": "user", "content": "作为一名营销专家，请为我的产品创作一个吸引人的口号"},
+        {"role": "assistant", "content": "当然，要创作一个吸引人的口号，请告诉我一些关于您产品的信息"},
+        {"role": "user", "content": "智谱AI开放平台"},
+        {"role": "assistant", "content": "点燃未来，智谱AI绘制无限，让创新触手可及！"},
+        {"role": "user", "content": "创作一个更精准且吸引人的口号"}
+    ],
+)
+print(response)
