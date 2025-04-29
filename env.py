fd32179c5c04989d04f3900c6575b66bb619357e
oyj <979109228@qq.com>
2025-04-29 18:00:01 +0800

    add study demo
    Signed-off-by: oyj <979109228@qq.com>

--- env.py
+++ env.py
@@ -2,3 +2,4 @@ import os
 os.environ["TAVILY_API_KEY"] = "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"
 os.environ["OPENAI_API_KEY"] = "sk-UG2ihQcsEyatiukR5Ah3FzL7L3TXTO8ttk0qdHaYTf0dLwfC"
 os.environ["OPENAI_BASE_URL"] = "https://api.chatanywhere.tech/v1"
+os.environ["huangface_key"] = "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"
